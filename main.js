30375cc517ac4f4d69e55ebf5ee9a2f9522b9e37
Tim <monkeyhut0@gmail.com>
2025-12-14 15:56:17 -0500

    changed to satellite map

--- main.js
+++ main.js
@@ -10,7 +10,12 @@ const map = L.map('map', {
 });
 
 // Add the base tile layer from openstreetmap
+/*
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+*/
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
