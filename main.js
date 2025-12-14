41070010394d380e2d10dbe5ec9b0744f748d387
Tim <monkeyhut0@gmail.com>
2025-12-14 17:01:05 -0500

    Setup popups for locations

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
+
+// import locations
+console.log("Fetching locations.json");
+fetch("./locations.json")
+    .then(response => response.json())
+    .then(data => {
+        const locations = data.locations;
+        for (const loc of locations ) {
+            console.log("Marking " + loc.name + "...")
+            var marker = L.marker(loc.coordinates).addTo(map);
+
+            // create popup
+            var popupElement = 
+                `<b>${loc.name}</b><br>
+                ${loc.description}`;
+            for (const img of loc.images) {
+                popupElement += `<br><img class="image" src="./images/${img.file}"><br>${img.year}`
+            }
+            marker.bindPopup(popupElement);
+        };
+    });
